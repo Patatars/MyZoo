deb7fde4d6ddcf536f2cb1f63d6de2974e7b435e
Patatars <101930305+Patatars@users.noreply.github.com>
2022-11-22 16:55:06 +0300

    addIsFeededProperty

--- Animal.py
+++ Animal.py
@@ -1,17 +1,36 @@
 class Animal:
-    def __init__(self, species: str, biome: str, needArea: int, food: list, predator: bool, sound: str):
-        self.species = species
-        self.biome = biome
-        self.needArea = needArea
-        self.food = food
-        self.predator = predator
-        self.sound = sound
+    def __init__(self, name, age, species, biome = "default", needArea = 1, foodTypes = [], predator = False, sound = "", foodVolume = 1):
+        self.__species = species
+        self.__biome = biome
+        self.__needArea = needArea
+        self.__foodTypes = foodTypes
+        self.__predator = predator
+        self.__isFeeded = False
+        self.__foodEated = 0
+        self.__sound = sound
+        self.__foodVolume = foodVolume
+        self.__name = name
+        self.__age = age
+
+
+
+    @property
+    def Feeded(self):
+        return self.__isFeeded
 
     def doSound(self):
-        print(self.sound)
+        print(self.__sound)
 
-    def eat(self):
-        print("Я ем")
+    def eat(self, foodType):
+        if foodType not in self.__foodTypes:
+            return
+        if self.__isFeeded:
+            return
+        if self.__foodEated >= self.__foodVolume:
+            self.__isFeeded = True
+            return
+        if foodType in self.__foodType:
+            self.__foodEated += 1
 
     def play(self):
         print("Я играю")
--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 from Animal import Animal
 
 if __name__ == '__main__':
-    pete = Animal("Слон", "Тропики", 10, ["Рыба"], False, "Ауф")
-    pete.name = "Петя"
-    pete.foodVolume = 5
-    pete.age = 5
-    simba = Animal("Пингвин", "Тундра", 10, ["Рыба", "Мясо"], False, "Хрю")
-    simba.name = "Симба"
-    simba.age = 1
-    matilde = Animal("Тигр", "Пустыня", 10, ["Рыба", "Мясо"], True, "Рррррр")
-    matilde.name = "Матильда"
-    matilde.foodVolume = 0.3
-    matilde.age = 20
+    pete = Animal("Петя", 5, "Слон", foodTypes=["Мясо", "Трава"], foodVolume=5)
+    simba = Animal("Симба",5, "Пингвин", biome="Тундра", needArea=10, foodTypes= ["Рыба", "Мясо"],sound= "Хрю", foodVolume=1)
+    matilde = Animal("Матильяда", 10, "Тигр", predator=True, foodVolume=2, foodTypes=["Мясо"])
+
 
     pete.doSound()
 
